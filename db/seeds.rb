a3046d2a781c1a2a89abedb1a1341383bfeaf6bd
Trestin <trestinishak@gmail.com>
2021-10-05 14:00:08 -0500

    added test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,7 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+User.create(name: "test1", username: "tester1", email: "test1@test.com", password: "password")
+User.create(name: "test2", username: "tester2", email: "test2@test.com", password: "password")
+
 puts "✅ Done seeding!"
