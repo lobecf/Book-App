6668e2f068a7b9bd72879d5f981dfbf8da6b7ebb
Trestin <trestinishak@gmail.com>
2021-10-06 11:35:16 -0500

    optimized code

--- react-app/my-app/src/CustomizePlaylist.js
+++ react-app/my-app/src/CustomizePlaylist.js
@@ -4,66 +4,24 @@ function CustomizePlaylist () {
 
     const genres = ["party", "self-discovery", "exercise"]
 
+    function createForms (section) {
+        const list = genres.map((genre, index) =>
+        <form>
+            <input key={index} type="checkbox" id={genre} name={genre} value={genre} />
+            <label for={genre}>{genre}</label>
+        </form>
+        )
+        return <div>
+            <h3>{section}</h3>
+            {list}
+            </div>
+    }
+
     return (
         <div className="customize-playlist-form">
             <h2>Welcome User</h2>
-            <h3>Pop</h3>
-            {/* <form> */}
-                {genres.map((genre, index) => 
-                <form>
-                    <input key={index} type="checkbox" id={genre} name={genre} value={genre} />
-                    <label for={genre}>{genre}</label>
-                </form>
-                )}
-                {/* <input type="checkbox" id="focus" name="focus" value="focus"/>
-                <label for="focus">Focus</label>
-                <input type="checkbox" id="party" name="party" value="party"/>
-                <label for="party">Party</label>
-                <input type="checkbox" id="self-discovery" name="self-discovery" value="self-disovery"/>
-                <label for="self-discovery">Self Discovery</label>
-                <input type="checkbox" id="exercise" name="exercise" value="exercise"/>
-                <label for="exercise">Exercise</label>
-                <input type="checkbox" id="stress-reducing" name="stress-reducing" value="stress-reducing"/>
-                <label for="stress-reducing">Stress Reducing</label>
-                <input type="checkbox" id="ease-pain" name="ease-pain" value="ease-pain"/>
-                <label for="ease-pain">Ease Pain</label>
-                <input type="checkbox" id="wellness" name="wellness" value="wellness"/>
-                <label for="wellness">Wellness</label> */}
-            {/* </form> */}
-            <h3>Rap/Hip Hop</h3>
-            <form>
-                <input type="checkbox" id="focus" name="focus" value="focus"/>
-                <label for="focus">Focus</label>
-                <input type="checkbox" id="party" name="party" value="party"/>
-                <label for="party">Party</label>
-                <input type="checkbox" id="self-discovery" name="self-discovery" value="self-disovery"/>
-                <label for="self-discovery">Self Discovery</label>
-                <input type="checkbox" id="exercise" name="exercise" value="exercise"/>
-                <label for="exercise">Exercise</label>
-                <input type="checkbox" id="stress-reducing" name="stress-reducing" value="stress-reducing"/>
-                <label for="stress-reducing">Stress Reducing</label>
-                <input type="checkbox" id="ease-pain" name="ease-pain" value="ease-pain"/>
-                <label for="ease-pain">Ease Pain</label>
-                <input type="checkbox" id="wellness" name="wellness" value="wellness"/>
-                <label for="wellness">Wellness</label>
-            </form>
-            <h3>Rock</h3>
-            <form>
-                <input type="checkbox" id="focus" name="focus" value="focus"/>
-                <label for="focus">Focus</label>
-                <input type="checkbox" id="party" name="party" value="party"/>
-                <label for="party">Party</label>
-                <input type="checkbox" id="self-discovery" name="self-discovery" value="self-disovery"/>
-                <label for="self-discovery">Self Discovery</label>
-                <input type="checkbox" id="exercise" name="exercise" value="exercise"/>
-                <label for="exercise">Exercise</label>
-                <input type="checkbox" id="stress-reducing" name="stress-reducing" value="stress-reducing"/>
-                <label for="stress-reducing">Stress Reducing</label>
-                <input type="checkbox" id="ease-pain" name="ease-pain" value="ease-pain"/>
-                <label for="ease-pain">Ease Pain</label>
-                <input type="checkbox" id="wellness" name="wellness" value="wellness"/>
-                <label for="wellness">Wellness</label>
-            </form>
+            {createForms("Pop")}
+            {createForms("Rap/Hip-Hop")}
         </div>
     )
 }
