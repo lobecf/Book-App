c4d8feaf5fbcbde0840abac8c5086135a949988e
Charles Lobe <lobecf@gmail.com>
2021-10-07 13:49:43 -0400

    update

--- react-app/my-app/src/GoalCard.js
+++ react-app/my-app/src/GoalCard.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function GoalCard() {
     const goals = ["Focus", "Party", "Self Discovery", "Exercise", "Improve Memory", "Reduce Stress", "Ease Pain", "Wellness"]
-    
+    let history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        history.push("/playlist")
+    }    
     function createGoalCard () {
         const list = goals.map((genre, index) =>
-        <div key={index}>
+        <div key={index} className="goal-card-container" onClick={handleSubmit}>
             <h3>{genre}</h3>
         </div>
         )
         return (
-        <div className="goal-card-container">
+        <div className="all-cards">
             {list}
         </div>
         )
