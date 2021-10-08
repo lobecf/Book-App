7148f27eb7b1efebb845abd53f939794827714ed
Trestin <trestinishak@gmail.com>
2021-10-07 19:56:33 -0500

    setting up api

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-require 'rspotify/oauth'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "<your_client_id>", "<your_client_secret>", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-end
--- react-app/my-app/src/Playlist.js
+++ react-app/my-app/src/Playlist.js
@@ -4,8 +4,10 @@ import { useLocation } from 'react-router-dom';
 function Playlist({ userInfo }) {
     const location = useLocation()
     const [goalsList, setGoalsList] = useState()
+    const [list, setList] = useState([])
 
-    const genre = location.state.genre.toLowerCase()
+    const goal = location.state.genre
+    console.log(goal)
     const API_KEY = process.env.REACT_APP_API_KEY
     const API_SECRET = process.env.REACT_APP_API_SECRET
 
@@ -15,14 +17,30 @@ function Playlist({ userInfo }) {
         .then(data => {
             console.log("Initial data", data)
             setGoalsList(data)
+            // const filteredGenres = data.map(element => {
+            //     const arr = element.goals.split(",")
+            //     return arr.filter(element => {
+            //         console.log(element)
+            //         return element === goal ? element : null
+            //     })
+            // })
+            // const allGenres = data.map(element => element.goals)
+            const findGenres = data.filter(element => element.goals.includes(goal))
+            const setGenres = findGenres.map(element => element.genres)
+            // console.log("Genres:", allGenres)
+            console.log("Includes Find:", setGenres)
+            console.log(`https://api.discogs.com/database/search?q=${goal}&genre&key=${API_KEY}&secret=${API_SECRET}`)
+            // console.log("Includes Filter:", filteredGenres)
+            // console.log(filteredGenres)
+            // const findGenres = data.filter(element => element.goals.includes(genre))
+            // genres.forEach(fetch using element .genre as interpolated piece)
         })
 
-        fetch(`https://api.discogs.com/database/search?q=${genre}&genre&key=${API_KEY}&secret=${API_SECRET}`)
+        fetch(`https://api.discogs.com/database/search?q=${goal}&genre&key=${API_KEY}&secret=${API_SECRET}`)
         .then(resp => resp.json())
         .then(data => console.log(data))
     }, [])
 
-    console.log(location.state.genre)
     return (
         <div>
             <p></p>
